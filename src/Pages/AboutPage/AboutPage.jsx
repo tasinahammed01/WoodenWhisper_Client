343ea34953df2124803a8d31236654e89ef1b48d
tasinahammed01 <tanjid.ahammed.01@gmail.com>
2025-05-29 16:41:27 +0600

    about section fully completed. also did responsive

--- src/Pages/AboutPage/AboutPage.jsx
+++ src/Pages/AboutPage/AboutPage.jsx
@@ -3,28 +3,29 @@ import React from "react";
 const AboutPage = () => {
   return (
     <div className="bg-[#EDE7DE]">
-      <div className="h-screen flex pt-[150px] px-5">
-        <div className="w-[50%] pr-8">
+      <div className="min-h-screen lg:flex pt-[100px] px-5 flex-col lg:flex-row">
+        <div className="lg:w-[50%] lg:pr-8 w-full">
           <p className="text-black text-xl leading-relaxed">Philosophy</p>
-          <h2 className="text-[60px] text-black leading-tight font-medium">
+          <h2 className="text-[40px] md:text-[50px] lg:text-[60px] text-black leading-tight font-medium">
             Where Design, Craftsmanship, and Vision Meet
           </h2>
-          <p className="text-[30px] text-black leading-snug">
+          <p className="text-[20px] md:text-[25px] lg:text-[30px] text-black leading-snug">
             At AMB, my design process is a journey—one that redefines
             possibilities through sketching, questioning, and constant dialogue
             with both clients and architects.
           </p>
         </div>
-        <div className="w-[50%]">
+        <div className="lg:w-[50%] w-full lg:mt-0 mt-10">
           <img
-            className="w-[100%] h-[90vh] object-cover"
+            className="w-full lg:h-[90vh] h-auto object-cover"
             src="https://i.ibb.co/yng7MSJF/Philosophy-Page01.jpg"
             alt="Philosophy"
           />
         </div>
       </div>
-      <div className="h-[80vh] flex flex-col justify-center text-black px-5 space-y-6">
-        <h1 className="text-[60px] leading-tight font-medium">
+
+      <div className="lg:min-h-[80vh] min-h-[50vh] flex flex-col justify-center text-black px-5 space-y-6 mt-20 lg:mt-0">
+        <h1 className="text-[30px] md:text-[40px] lg:text-[60px] leading-tight font-medium">
           I crave uniquely tangible qualities in everything I create, where
           materials, shapes, textures, and patinas come together to evoke
           something thrilling and emotionally binding.
@@ -39,11 +40,17 @@ const AboutPage = () => {
           the raw and the refined.
         </p>
       </div>
-      <div>
-        <img src="https://i.ibb.co/Tx1hCS5V/Philosophy-Page02-1.jpg" alt="" />
+
+      <div className="w-full mt-10">
+        <img
+          className="w-full object-cover"
+          src="https://i.ibb.co/Tx1hCS5V/Philosophy-Page02-1.jpg"
+          alt=""
+        />
       </div>
-      <div className="h-[80vh] flex flex-col justify-center text-black px-5 space-y-6">
-        <h1 className="text-[60px] leading-tight font-medium">
+
+      <div className="lg:min-h-[80vh] min-h-[50vh]  flex flex-col justify-center text-black px-5 space-y-6 mt-10">
+        <h1 className="text-[30px] md:text-[40px] lg:text-[60px] leading-tight font-medium">
           Design is never an isolated act; it emerges from a moving dialogue
           amongst builders, clients, and architects as we aim to achieve the
           vision through cutting-edge design.
@@ -62,6 +69,7 @@ const AboutPage = () => {
           we live and feel.
         </p>
       </div>
+
       <div className="flex justify-center py-16 px-5">
         <div className="w-full">
           <video
@@ -74,20 +82,56 @@ const AboutPage = () => {
           </video>
         </div>
       </div>
-      <div className="h-[60vh] flex flex-col justify-center items-center space-y-[60px] text-black px-5 space-y-6">
-        <h1 className="text-[60px] leading-tight font-medium text-center w-[50%] ">
+
+      <div className="min-h-[60vh] flex flex-col justify-center items-center text-black px-5 space-y-6">
+        <h1 className="text-[30px] md:text-[40px] lg:text-[60px] leading-tight font-medium text-center w-full md:w-[80%] lg:w-[50%]">
           The home is a vessel where we collect experiences and share them with
           those who are a part of our lives.
         </h1>
-        <div className="lg:w-[8%] md:w-[15%] w-[30%]">
+        <div className="w-[60%] md:w-[30%] lg:w-[8%]">
           <button className="unique-btn2 w-full">
-            <span className="text-black title-transition2 active">OUR JOURNAL</span>
+            <span className="text-black title-transition2 active">
+              OUR JOURNAL
+            </span>
             <span className="arrow">
               <svg viewBox="0 0 24 24">
                 <path d="M5 12h14M12 5l7 7-7 7" />
               </svg>
             </span>
           </button>
+        </div>
+      </div>
+
+      <div className="flex flex-col lg:flex-row">
+        <div className="flex justify-center py-16 px-5 w-full lg:w-[50%]">
+          <div className="w-full">
+            <video
+              className="w-full rounded-lg shadow-lg"
+              controls
+              poster="https://i.ibb.co/Kpw0DQ5F/Philosophy-Page03-poster.jpg"
+            >
+              <source src="your-video-file.mp4" type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+            <p className="text-[16px] md:text-[18px] text-black mt-10 leading-relaxed max-w-full lg:max-w-[40%] mr-auto text-left font-medium">
+              A sense of peace must be delivered the moment you walk through the
+              door — this is the ultimate luxury. I design spaces that bring a
+              sense of belonging, where both residents and friends can indulge
+              in meaningful and memorable exchanges.
+            </p>
+          </div>
+        </div>
+        <div className="flex justify-center py-16 px-5 w-full lg:w-[50%]">
+          <div className="w-full">
+            <video
+              className="w-full rounded-lg shadow-lg"
+              controls
+              poster="https://i.ibb.co/JWnwW2Q9/Philosophy-Page04-poster.jpg"
+            >
+              <source src="your-video-file.mp4" type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
         </div>
       </div>
     </div>
